670fc15f311af4f50fa40f7c3754f41e9562bc98
Joseph McKee <MCKEJI2@student.op.ac.nz>
2024-04-10 00:40:48 +1200

    Created Person class and person constructer.

---
+++ Vaughns_Amasing_Business/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaughns_Amasing_Business
+{
+    public class Person
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private string phone;
+        private DateTime date;
+        private string email;
+
+        public Person(int id, string firstName, string lastName, string phone, DateTime date, string email)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phone = phone;
+            this.date = date;
+            this.email = email;
+        }
+    }
+}

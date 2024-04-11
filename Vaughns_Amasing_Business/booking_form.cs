fc4738a948c4172c0fa62c92b7ff0752bf0c1b05
loganb294 <BOYDLP1@student.op.ac.nz>
2024-04-11 12:54:12 +1200

    booking UI done

---
+++ Vaughns_Amasing_Business/booking_form.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vaughns_Amasing_Business
+{
+    public partial class booking_form : Form
+    {
+        public booking_form()
+        {
+            InitializeComponent();
+        }
+
+        private void booking_form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Fname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Phone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Email_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Data_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
